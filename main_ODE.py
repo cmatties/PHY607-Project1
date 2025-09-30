cfb9c6efad6b6cac93a94433addfa1d58fd6469c
Carter <cmatties2003@gmail.com>
2025-09-30 09:02:10 -0400

    Fix solving for parameters C1, C2 in exact solution

--- main_ODE.py
+++ main_ODE.py
@@ -18,6 +18,7 @@ def reference_ODE(
     """
     omega_prime = (k / m - (gamma / 2) ** 2) ** 0.5
     phi = (omega * gamma) / (k / m - omega**2)
+    particular_coefficient = (F0 / m)* ((k / m - omega**2) ** 2 + omega**2 * gamma**2) ** -0.5
     intial_conditions_matrix = np.array(
         [
             [
@@ -38,14 +39,12 @@ def reference_ODE(
             ],
         ]
     )
-    initial_conditions_vector = np.array([x0, v0])
+    initial_conditions_vector = np.array([x0-particular_coefficient*np.cos(omega*t0-np.arctan(phi)), v0+omega*particular_coefficient*np.sin(omega*t0-np.arctan(phi))])
     C1, C2 = np.linalg.solve(intial_conditions_matrix, initial_conditions_vector)
     return (
         C1 * np.exp(-gamma / 2 * t) * np.cos(omega_prime * t)
         + C2 * np.exp(-gamma / 2 * t) * np.sin(omega_prime * t)
-        + (F0 / m)
-        * ((k / m - omega**2) ** 2 + omega**2 * gamma**2) ** -0.5
-        * np.cos(omega * t - np.arctan(phi))
+        + particular_coefficient * np.cos(omega * t - np.arctan(phi))
     )
     
 
@@ -55,3 +54,31 @@ def xdot(x, v, t):
 
 def vdot(x, v, t, k=k_ODE, m=m_ODE, F0=F0_ODE, omega=omega_ODE, gamma=gamma_ODE):
     return F0 / m * np.cos(omega * t) - (k / m) * x - gamma * v
+    
+    
+#Plot global error
+error_list_Euler = []
+error_list_RK4 = []
+dt_list = np.linspace(1e-4, 1, num=100)
+for dt in dt_list:
+    xlist_Euler, vlist_Euler, tlist_Euler = myODE.solve(
+        myODE.Euler, 0, 3, xdot, vdot, dt, 0, 10
+    )
+    xlist_RK4, vlist_RK4, tlist_RK4 = myODE.solve(
+        myODE.RK4, 0, 3, xdot, vdot, dt, 0, 10
+    )
+    x_final_Euler = xlist_Euler[-1]
+    x_final_RK4 = xlist_RK4[-1]
+    x_final_exact = reference_ODE(tlist_Euler[-1], 0, 3, 0)
+
+    error_list_Euler.append(np.abs(x_final_Euler - x_final_exact))
+    error_list_RK4.append(np.abs(x_final_RK4 - x_final_exact))
+
+plt.plot(dt_list, error_list_Euler, label="Euler")
+plt.plot(dt_list, error_list_RK4, label="RK4")
+plt.legend()
+plt.title("Global Truncation Error", size = 20)
+plt.xlabel("Timestep", size = 13)
+plt.ylabel(r"$|x(t_{\mathrm{final}})-x_{\mathrm{exact}}(t_{\mathrm{final}})|$", size = 13)
+plt.show()
+
