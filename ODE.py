8d1f3cb06f2891c561c1120e17a396eaf190a69b
Carter <cmatties2003@gmail.com>
2025-09-30 09:03:11 -0400

    Fix iterating through timesteps in solve method

--- ODE.py
+++ ODE.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def solve(integrator, x0, v0, xdot, vdot, dt, t0, t_max):
     """Use the given integrator to solve the differential equation
     given by the functions xdot(x,v,t) and vdot(x,v,t),
@@ -12,7 +14,7 @@ def solve(integrator, x0, v0, xdot, vdot, dt, t0, t_max):
     x = x0
     v = v0
 
-    for t in range(t0, t_max, dt):
+    for t in np.arange(t0, t_max, dt):
         x_new, v_new = integrator(x, v, xdot, vdot, t, dt)
         x_list.append(x_new)
         v_list.append(v_new)
